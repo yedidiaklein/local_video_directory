5359baf596a4b9a0be4b17a111a887a70f3133f2
Yedidia Klein <yedidia@openapp.co.il>
2017-04-03 09:32:05 +0300

    code style fixing

--- ajax_edit.php
+++ ajax_edit.php
@@ -1,17 +1,14 @@
 <?php
-
-
-// This script has to get more security...
-// first log to moodle log who did, when and what were fields value before an after
+defined('MOODLE_INTERNAL') || die();
 
 require_once('init.php');
 
 $PAGE->set_context(context_system::instance());
 
-$id = 		required_param('id', PARAM_INT);
-$status = 	optional_param('status', 0 ,PARAM_BOOL);
-$value  =	optional_param('value', "" ,PARAM_RAW);
-$field  =	required_param('field', PARAM_RAW);
+$id = required_param('id', PARAM_INT);
+$status = optional_param('status', 0 ,PARAM_BOOL);
+$value  = optional_param('value', "" ,PARAM_RAW);
+$field  = required_param('field', PARAM_RAW);
 
 if ($value != "") {
 	$record = array("id" => $id, $field => urldecode($value));	
@@ -22,4 +19,3 @@ if ($value != "") {
 if ($update = $DB->update_record("local_video_directory",$record)) {
 	echo '1';
 }
-
