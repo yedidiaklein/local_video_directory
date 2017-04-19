900448143992ada5035f2ccc38fa0fa0483a8f1a
Yedidia Klein <yedidia@openapp.co.il>
2017-04-19 16:47:49 +0300

    version up

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017040403;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2017041901;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;      // Requires this Moodle version.
 $plugin->component = 'local_video_directory';// Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "0.92 (Build - 2017040403)";
+$plugin->release = "0.92 (Build - 2017041901)";
 $plugin->cron      = 1;
