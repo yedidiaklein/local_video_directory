1fa0be6bd4d31b209304e49b89edbed2dc9fee5f
Yedidia Klein <yedidia@openapp.co.il>
2017-04-20 14:34:34 +0300

    version updated

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2017041901;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2017042001;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;      // Requires this Moodle version.
 $plugin->component = 'local_video_directory';// Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "0.92 (Build - 2017041901)";
+$plugin->release = "0.92 (Build - 2017042001)";
 $plugin->cron      = 1;
