b39ce41d6dcee3c567b754f26eed30bb8582068d
Yedidia Klein <yedidia@openapp.co.il>
2017-06-08 20:52:56 +0300

    allow multi files upload

--- upload.php
+++ upload.php
@@ -23,58 +23,85 @@ require_once('init.php');
 defined('MOODLE_INTERNAL') || die();
 
 require_once("$CFG->libdir/formslib.php");
+require_once($CFG->dirroot."/repository/lib.php");
+
+$streaming_url = $settings->streaming;
 
 $PAGE->set_context(context_system::instance());
 $PAGE->set_heading(get_string('upload','local_video_directory'));
 $PAGE->set_title(get_string('upload','local_video_directory'));
 $PAGE->set_url('/local/video_directory/upload.php');
-$PAGE->set_pagelayout('standard');
-
 $PAGE->navbar->add(get_string('pluginname','local_video_directory'), new moodle_url('/local/video_directory/'));
 $PAGE->navbar->add(get_string('upload','local_video_directory'));
 $PAGE->requires->css('/local/video_directory/style.css');
+$PAGE->set_context(context_system::instance());
+$context = context_user::instance($USER->id);
+
 
 class simplehtml_form extends moodleform {
+    //Add elements to form
     public function definition() {
-        global $CFG, $DB;
-        $mform = $this->_form;
+        global $CFG, $DB, $context; 
+ 
+        	$mform = $this->_form; // Don't forget the underscore! 
+  
+ 			$mform->addElement('checkbox', 'private', get_string('private', 'local_video_directory'));
+			$mform->setDefault('private','checked');
+			$mform->addElement('filemanager', 'attachments', get_string('file', 'moodle'), null,
+                    array('subdirs' => 3, 'areamaxbytes' => -1, 'maxfiles' => 50,
+                          'accepted_types' => array('audio','video'), 'return_types'=> FILE_INTERNAL | FILE_EXTERNAL));
 
-        $mform->addElement('checkbox', 'private', get_string('private', 'local_video_directory'));
-        $mform->setDefault('private','checked');
-
-        $mform->addElement('filepicker', 'userfile', get_string('file'), null, array('maxbytes' => 1000000000, 'accepted_types' => '*'));
-        
-        $buttonarray=array();
-        $buttonarray[] = $mform->createElement('submit', 'submitbutton', get_string('savechanges'));
-        $buttonarray[] = $mform->createElement('cancel', 'cancel', get_string('cancel'));
-        $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
-            
+ 			if (empty($entry->id)) {
+    			$entry = new stdClass;
+    			$entry->id = null;
+			}
+ 
+			$draftitemid = file_get_submitted_draft_itemid('attachments');
+  
+			file_prepare_draft_area($draftitemid, $context->id, 'mod_glossary', 'attachment', $entry->id,
+                        array('subdirs' => 3, 'maxfiles' => 50));
+ 
+			$entry->attachments = $draftitemid;
+      		$buttonarray=array();
+			$buttonarray[] =& $mform->createElement('submit', 'submitbutton', get_string('savechanges'));
+			$buttonarray[] =& $mform->createElement('cancel', 'cancel', get_string('cancel'));
+			$mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+			
     }
-    
+    //Custom validation should be added here
     function validation($data, $files) {
         return array();
     }
 }
 
+//Instantiate simplehtml_form 
 $mform = new simplehtml_form();
  
+//Form processing and displaying is done here
 if ($mform->is_cancelled()) {
+    //Handle form cancel operation, if cancel button is present on form
     redirect($CFG->wwwroot . '/local/video_directory/list.php');
 } else if ($fromform = $mform->get_data()) {
-    $name = $mform->get_new_filename('userfile');
-    
-    $record = array('orig_filename' => $name , 'owner_id' => $USER->id);
-    if ((isset($fromform->private)) && ($fromform->private)) {
-        $record['private'] = 1;
-    }
-
-    $lastinsertid = $DB->insert_record('local_video_directory', $record);
-    $success = $mform->save_file('userfile', $uploaddir.$lastinsertid);
-    redirect($CFG->wwwroot . '/local/video_directory/list.php');
+  //In this case you process validated data. $mform->get_data() returns data posted in form.
+  
+  	$files=$DB->get_records_select('files',"itemid = $fromform->attachments and filename <> '.'",null,'contenthash,filename');
+	foreach ($files as $file) {
+		$record=array('orig_filename' => $file->filename , 'owner_id' => $USER->id	);
+		if ((isset($fromform->private)) && ($fromform->private)) {
+			$record['private'] = 1;
+		}
+		$lastinsertid = $DB->insert_record('local_video_directory', $record);
+		$path = substr($file->contenthash, 0,2) ."/" . substr($file->contenthash, 2 , 2)."/";
+		copy($CFG->dataroot."/filedir/".$path.$file->contenthash,$uploaddir.$lastinsertid);		
+	}
+  
+	  
+  	redirect($CFG->wwwroot . '/local/video/list.php');
 } else {
-    echo $OUTPUT->header();
-          
-    require 'menu.php';
+  //displays the form
+	echo $OUTPUT->header();
+	//Menu
+	include('menu.php');  		
     $mform->display();
 }
 
