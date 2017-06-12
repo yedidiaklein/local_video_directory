797ad028be5ed584c985710ed0608bbb7f088311
Yedidia Klein <yedidia@openapp.co.il>
2017-06-12 23:25:45 +0300

    code style

--- play.php
+++ play.php
@@ -27,54 +27,54 @@ $id = required_param('video_id', PARAM_INT);
 $file = "$converted$id.mp4";
 
 
-$fp = @fopen($file, 'rb');      
-$size = filesize($file); // File size 
-$length = $size;           // Content length
-$start = 0;               // Start byte
-$end = $size - 1;       // End byte  
+$fp = @fopen($file, 'rb');
+$size = filesize($file); // File size.
+$length = $size; // Content length.
+$start = 0; // Start byte.
+$end = $size - 1; // End byte.
 header('Content-type: video/mp4');
 header("Accept-Ranges: 0-$length");
-header("Accept-Ranges: bytes"); 
+header("Accept-Ranges: bytes");
 if (isset($_SERVER['HTTP_RANGE'])) {
-    $cStart = $start;              
-    $cEnd   = $end;                
+    $cstart = $start;
+    $cend   = $end;
     list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
     if (strpos($range, ',') !== false) {
         header('HTTP/1.1 416 Requested Range Not Satisfiable');
         header("Content-Range: bytes $start-$end/$size");
         header("X-Data: filename $file");
-        exit;      
-    }              
-    if ($range == '-') {            
-        $cStart = $size - substr($range, 1);
-    }else{         
-        $range = explode('-', $range); 
-        $cStart = $range[0];           
-        $cEnd = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+        exit;
     }
-    $cEnd = ($cEnd > $end) ? $end : $cEnd;
-    if ($cStart > $cEnd || $cStart > $size - 1 || $cEnd >= $size) { 
+    if ($range == '-') {
+        $cstart = $size - substr($range, 1);
+    } else {
+        $range = explode('-', $range);
+        $cstart = $range[0];
+        $cend = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+    }
+    $cend = ($cend > $end) ? $end : $cend;
+    if ($cstart > $cend || $cstart > $size - 1 || $cend >= $size) {
         header('HTTP/1.1 416 Requested Range Not Satisfiable');
         header("Content-Range: bytes $start-$end/$size");
         header("X-Data: filename $file");
-        exit;      
+        exit;
     }
-    $start = $cStart;             
-    $end = $cEnd;               
-    $length = $end - $start + 1;    
-    fseek($fp, $start);             
+    $start = $cstart;
+    $end = $cend;
+    $length = $end - $start + 1;
+    fseek($fp, $start);
     header('HTTP/1.1 206 Partial Content');
-}                  
+}
 header("Content-Range: bytes $start-$end/$size");
 header("Content-Length: ".$length);
 $buffer = 1024 * 8;
-while(!feof($fp) && ($p = ftell($fp)) <= $end) { 
-    if ($p + $buffer > $end) {      
-        $buffer = $end - $p + 1;        
-    }              
-    set_time_limit(0);              
-    echo fread($fp, $buffer);       
-    ob_flush();    
+while (!feof($fp) && ($p = ftell($fp)) <= $end) {
+    if ($p + $buffer > $end) {
+        $buffer = $end - $p + 1;
+    }
+    set_time_limit(0);
+    echo fread($fp, $buffer);
+    ob_flush();
 }
-fclose($fp);       
+fclose($fp);
 exit();
