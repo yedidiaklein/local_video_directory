80f38b5fa37aa3c0fc1ee8da6009ff34209e5c90
Yedidia Klein <yedidia@openapp.co.il>
2017-12-25 17:54:01 +0200

    bugfix

--- list.php
+++ list.php
@@ -29,18 +29,12 @@ $settings = get_settings();
 if (!CLI_SCRIPT) {
     require_login();
 
-    // Check if user belong to the cohort or is admin.
-    require_once($CFG->dirroot.'/cohort/lib.php');
-
     // Check if user have permissionss.
     $context = context_system::instance();
     if (!has_capability('local/video_directory:video', $context) && !is_siteadmin($USER)) {
-        die("Access Denied. You must be a member of the designated cohort. Please see your site admin.");
+        die("Access Denied. You must get rights... Please see your site admin.");
     }
 
-    // if (!cohort_is_member($settings->cohort, $USER->id) && !is_siteadmin($USER)) {
-    // die("Access Denied. You must be a member of the designated cohort. Please see your site admin.");
-    // }
 }
 
 $tags = optional_param('tag', 0, PARAM_RAW);
--- upload.php
+++ upload.php
@@ -42,9 +42,6 @@ if (!CLI_SCRIPT) {
         die("Access Denied. You must be a member of the designated cohort. Please see your site admin.");
     }
 
-    // if (!cohort_is_member($settings->cohort, $USER->id) && !is_siteadmin($USER)) {
-    // die("Access Denied. You must be a member of the designated cohort. Please see your site admin.");
-    // }
 }
 
 $PAGE->set_context(context_system::instance());
