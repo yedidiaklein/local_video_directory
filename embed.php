28e921096dfaaa7dc15d9b0f38e638fb18b24a3b
Yedidia Klein <yedidia@openapp.co.il>
2018-12-26 23:09:05 +0200

    typo

--- embed.php
+++ embed.php
@@ -32,7 +32,7 @@ if (!$config->allowanonymousembed) {
 }
 
 $PAGE->set_context(context_system::instance());
-$PAGE->requires->css('/local/video_directory/styles/video-js.min.css'));
+$PAGE->requires->css('/local/video_directory/styles/video-js.min.css');
 $PAGE->set_pagelayout('embedded');
 $PAGE->set_url('/local/video_directory/embed.php');
 
