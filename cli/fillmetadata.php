97cceb4d94ef3c4b99e40cf69253028395550184
Yedidia Klein <yedidia@openapp.co.il>
2019-12-05 01:43:34 +0200

    category in menu

--- cli/fillmetadata.php
+++ cli/fillmetadata.php
@@ -7,31 +7,31 @@ $settings = get_settings();
 $dirs = get_directories();
 $converted = $dirs['converted'];
 
-$ffprobe = $settings -> ffprobe;
+$ffprobe = $settings->ffprobe;
 
 
-$videos = $DB->get_records('local_video_directory',array("height" => NULL));
+$videos = $DB->get_records('local_video_directory', array("height" => NULL));
 foreach ($videos as $video) {
 
-			if (file_exists($converted . $video->id . ".mp4")) {
-				echo "processing :".$video->id."\n";
-				$metadata=array();
-				$metafields = array("height" => "stream=height", "width" => "stream=width", "size" => "format=size");
-				foreach ($metafields as $key => $value) {
-					$metadata[$key] = exec($ffprobe . " -v error -show_entries " . $value . " -of default=noprint_wrappers=1:nokey=1 " . $converted . $video->id . ".mp4"); 
-				}
-	
-				// update that converted and streaming URL
-				$record = array("id" => $video->id, 
-							"height" => $metadata['height'],
-							"width" => $metadata['width'],
-							"size" => $metadata['size'],
-							"timecreated" => time(),
-							"timemodified" => time(),
-							'uniqid' => uniqid('', true)
-							);
+    if (file_exists($converted . $video->id . ".mp4")) {
+        echo "processing :".$video->id."\n";
+        $metadata = array();
+        $metafields = array("height" => "stream=height", "width" => "stream=width", "size" => "format=size");
+        foreach ($metafields as $key => $value) {
+            $metadata[$key] = exec($ffprobe . " -v error -show_entries " . $value . " -of default=noprint_wrappers=1:nokey=1 " . $converted . $video->id . ".mp4"); 
+        }
 
-				$update = $DB->update_record("local_video_directory",$record);
-				print_r($record);
-			}
+        // Update that converted and streaming URL.
+        $record = array("id" => $video->id, 
+                        "height" => $metadata['height'],
+                        "width" => $metadata['width'],
+                        "size" => $metadata['size'],
+                        "timecreated" => time(),
+                        "timemodified" => time(),
+                        'uniqid' => uniqid('', true)
+                        );
+
+        $update = $DB->update_record("local_video_directory", $record);
+        print_r($record);
+    }
 }
